e6bc8276b2761376e07370cdae94c4a6bbe6a4e9
nbngocn188-art <nbngocn188@gmail.com>
2026-05-15 13:36:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
-@app.router('/')
+@app.route("/")
 def home():
-  return f"Hello World from {os.getenv('RAILWAY_REPLICA_ID')}"
-app.run(host='0.0.0.0', port=8080)
+  return "Hello World"
